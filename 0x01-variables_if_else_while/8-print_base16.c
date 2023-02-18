f9f6dfef7bb9b1a4065b00361380b5719c58090b
Amaebi Amara <jonkbog@gmail.com>
2023-02-18 08:34:16 -0800

    modified 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,33 +7,14 @@
 int main(void)
 {
 	int i;
-	int j;
-	int iter;
-	int int_array[10];
-	int char_array[7];
-	int ascii_txt = 97;
-	int size_int;
-	int size_char;
-	int size_array;
-	
-	for (i = 194; i >= 185; i--)
+	i = 0;
+	while(i < 104)
 	{
-		/*  Writes the equivalent character */
-		iter = 194 - i;
-		int_array[iter] = iter;
-		if (iter < 6)
-			char_array[iter] = ascii_txt + iter;
-		putchar('0' + iter);
-	}
-	size_int = (sizeof(char_array) / sizeof(char_array[0]));
-	size_char = (sizeof(int_array) / sizeof(int_array[0]));
-	size_array = size_int > size_char ? size_int : size_char;
-	j = 0;
-	while (j < size_array)
-	{
-		if (j < size_char)
-			putchar(char_array[j]);
-		j++;
+		if (i < 10)
+			putchar('0' + i);
+		else if (i > 96 && i < 103)
+			putchar(i);
+		i++;
 	}
 	putchar('\n');
 	return (0);
