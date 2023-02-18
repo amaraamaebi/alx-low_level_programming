155c5f68a24aa9d39849b7e64ea07e0a91b56b8b
Amaebi Amara <jonkbog@gmail.com>
2023-02-17 20:11:45 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+
+
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %c  %s" , n, n, s)
+	return (0);
+}
