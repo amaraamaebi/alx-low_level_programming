7fd9b6511c264e3fefd6fb2a36faa6c7bcd7e782
Amaebi Amara <jonkbog@gmail.com>
2023-02-17 21:41:21 -0800

    2-print_alphabet.c added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha)
+        return (0);
+}
