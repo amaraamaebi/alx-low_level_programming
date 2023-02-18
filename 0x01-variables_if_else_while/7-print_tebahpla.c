94e2e911412677d634b51489d813484f05e1d63b
Amaebi Amara <jonkbog@gmail.com>
2023-02-17 23:27:36 -0800

    added 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha = 'a'; alpha--)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
