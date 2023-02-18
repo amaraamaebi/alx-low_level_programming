5c6afb5af61b23e0d2241d55d9d9b5972078315d
Amaebi Amara <jonkbog@gmail.com>
2023-02-18 07:15:51 -0800

    modded 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,34 +6,34 @@
  */
 int main(void)
 {
-        int i;
-        int j;
-        int iter;
-        int int_array[10] = { };
-        int char_array[7] = { };
-        int ascii_txt = 97;
-        int size_int;
-        int size_char;
-        int size_array;
+	int i;
+	int j;
+	int iter;
+	int int_array[10];
+	int char_array[7];
+	int ascii_txt = 97;
+	int size_int;
+	int size_char;
+	int size_array;
 
-        for (i = 194; i >= 185; i--)
-        {
-                /*  Writes the equivalent character */
-                iter = 194 - i;
-                int_array[iter] = iter;
-                if (iter < 6)
-                        char_array[iter] = ascii_txt + iter;
-                putchar('0' + iter);
-        }
-        size_int = (sizeof(char_array) / sizeof(char_array[0]));
-        size_char = (sizeof(int_array) / sizeof(int_array[0]));
-        size_array = size_int > size_char ? size_int : size_char;
-        j = 0;
-        while (j < size_array)
-        {
-                if (j < size_char)
-                        putchar(char_array[j]);
-                j++;
-        }
-        return (0);
+       	for (i = 194; i >= 185; i--)
+       	{
+		/*  Writes the equivalent character */
+		iter = 194 - i;
+		int_array[iter] = iter;
+		if (iter < 6)
+			char_array[iter] = ascii_txt + iter;
+		putchar('0' + iter);
+	}
+	size_int = (sizeof(char_array) / sizeof(char_array[0]));
+	size_char = (sizeof(int_array) / sizeof(int_array[0]));
+	size_array = size_int > size_char ? size_int : size_char;
+	j = 0;
+	while (j < size_array)
+	{
+		if (j < size_char)
+			putchar(char_array[j]);
+		j++;
+	}
+	return (0);
 }
