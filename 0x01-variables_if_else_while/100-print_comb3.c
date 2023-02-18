9b565ee9c6154671406f842573ab56948cf7200d
Amaebi Amara <jonkbog@gmail.com>
2023-02-18 09:56:12 -0800

    added 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 1;
+	while (i < 9)
+	{
+		putchar('0' + i);
+		putchar('0' + j);
+		if (i < 8)
+		{
+			putchar(44);
+			putchar(32);
+		}
+
+		if (j > 8)
+		{
+			i++;
+		}
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
