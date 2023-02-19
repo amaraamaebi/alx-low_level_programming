31b49ca6308f9a1663bcb01585a210113c86648f
Amaebi Amara <jonkbog@gmail.com>
2023-02-19 05:21:28 -0800

    moddedd 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	i = 0;
+	j = 1;
+	k = 2;
+	while (i < 8)
+	{
+		putchar('0' + i);
+		putchar('0' + j);
+		putchar('0' + k);
+		if (i < 7)
+		{
+			putchar(44);
+			putchar(32);
+		}
+
+		if (k > 8)
+		{
+			j = j + 1;
+			k = j + 1;
+		}
+
+		if (j > 7)
+			i++;
+		k++;
+	}
+	putchar('\n');
+	return (0);
+}
