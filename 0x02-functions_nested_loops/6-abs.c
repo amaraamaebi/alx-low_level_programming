deedb41f18e2d4533a5c26ea19b1bd7a94a361a3
Amaebi Amara <jonkbog@gmail.com>
2023-02-22 14:10:49 -0800

    absolut

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @n: integer to print
+ * Return: 1 on (Success)
+ */
+int _abs(int n)
+{
+	if (i < 0)
+		return (i * -1);
+	else
+		return (i);
+}
