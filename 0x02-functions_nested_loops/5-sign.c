c4d28338fdebbeb7a4fa630d4d3adcb33352f504
Amaebi Amara <jonkbog@gmail.com>
2023-02-22 13:55:11 -0800

    create 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * @n: integer to print
+ * Return: 1 on (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
