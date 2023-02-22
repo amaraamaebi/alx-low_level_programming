48cef35cb1ba712a1d524bf48a2d64b8b24e361a
Amaebi Amara <jonkbog@gmail.com>
2023-02-22 10:31:27 -0800

    putchar print newline

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
