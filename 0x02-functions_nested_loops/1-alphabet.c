5d2a1dbb7840729c46e48e74d886b7c3b2b5efdf
Amaebi Amara <jonkbog@gmail.com>
2023-02-22 12:54:30 -0800

    prototype

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,19 +2,6 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet - Entry point
  *
  * Return: Always 0 (Success)
