c1875dc329f3585aa37873359f5d074c3c09ac22
Amaebi Amara <jonkbog@gmail.com>
2023-02-23 16:58:08 -0800

    created debugging directory

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+/* more headers goes there
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+* betty style doc for function main goes there */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i)
+	return (0);
+}
