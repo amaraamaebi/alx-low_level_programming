14fb0f1c839583b63fbe989ad42ec15e9dc2fdf6
Amaebi Amara <jonkbog@gmail.com>
2023-03-05 02:20:52 -0800

    create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - void
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		_putchar(i);
+	}
+	_putchar('\n');
+}
