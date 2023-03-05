82cb4eb7bbb0c25d76b11c83a3e8a5c1774f8388
Amaebi Amara <jonkbog@gmail.com>
2023-03-05 07:40:35 -0800

    create more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - void
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 11; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j < 10)
+			{
+				_putchar(j);
+			}
+			else
+			{
+				_putchar('0' + j);
+			}
+		}
+		_putchar('\n');
+	}
+}
