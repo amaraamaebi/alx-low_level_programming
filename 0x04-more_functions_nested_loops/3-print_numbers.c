a787ffdace75e7d0c4ccedc857f249456cfabf2d
Amaebi Amara <jonkbog@gmail.com>
2023-03-05 01:58:15 -0800

    commit more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - void
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	return (void);
+}
