e3b2ad1d44b892d1e933c39263abc96e9e47c85e
Amaebi Amara <jonkbog@gmail.com>
2023-03-05 01:28:20 -0800

    ceate mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - mul(int a, int b).
+ * @a: variable collect.
+ * @b: variable collect.
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+	return sum;
+}
