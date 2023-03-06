a19390795c464bd9693359ca6186f5f504ff6170
Amaebi Amara <jonkbog@gmail.com>
2023-03-06 02:33:25 -0800

    9-fizz_buzz.c created

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - void
+ * variable to print.
+ * Return: Always 0.
+ */
+void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+			if (i % 5 == 0)
+				printf("Buzz");
+			putchar(32);
+		}
+		else if (i % 5 == 0)
+		{
+			if (i % 3 == 0)
+				printf("Fizz");
+			printf("Buzz");
+			putchar(32);
+		}
+		else
+		{
+			printf("%d", i);
+			putchar(32);
+		}
+	}
+}
